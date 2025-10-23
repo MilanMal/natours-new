0c5e9f48c7f88b3f04ffa1f749bd17b7e037f03e
Milan Mal <milanmal75@gmail.com>
2025-10-23 13:04:22 +0530

    comment

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDDLEWARES
-
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
